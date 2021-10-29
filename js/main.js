c79790761f23d138f8022097d1c4e9a671a0d506
Erin Hancock <erinhancock794@gmail.com>
2021-10-28 20:38:45 -0600

    added event listeners to buttons, cards sort into correct container

--- js/main.js
+++ js/main.js
@@ -1,110 +1,229 @@
 (function (window) {
-const theList = document.querySelector('#theList');
-let formattedSearchResults = []
+  // const theList = document.querySelector('#theList');
+  let formattedSearchResults = [];
 
-fetch('http://openlibrary.org/search.json?q=the+lord+of+the+rings&limit=6')
-.then(res => res.json())
-.then(data => {
-    console.log('data--->', data);
-    // data.docs.forEach(book => addToList(book.title))
-    data.docs.forEach(book => {
-        compileBookData(book);
-        // addDataToCard(formattedSearchResults)
-    })
+  const searchInput = document.querySelector(".form-input");
+  searchInput.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      fetchAPIData(searchInput);
+    }
+  });
 
-    // return data.docs[0]
-    formattedSearchResults.forEach(book => addDataToCard(book))
-})
-.then(theFirstBook => {
-    console.log(theFirstBook);
-});
+  function fetchAPIData(searchInput) {
+    let searchInputValue = searchInput.value;
+    console.log("searchInputValue---->", searchInputValue);
+    fetch(`http://openlibrary.org/search.json?q=${searchInputValue}&limit=5`)
+      .then((res) => res.json())
+      .then((data) => {
+        data.docs.forEach((book) => {
+          compileBookData(book);
+        });
+        formattedSearchResults.forEach((book) => addDataToCard(book, 'search'));
+      });
+  }
 
-
-
-function compileBookData(book) {
-
+  function compileBookData(book) {
     const author = book.author_name;
     const bookInfo = {
-        key: book.key,
-        title: book.title,
-        author: author || 'unknown',
-        publishYear: book.first_publish_year || '',
-        // isbn: book.isbn,
-        // lccn: book.lccn
+        id: book.edition_key[0],
+      key: book.key,
+      title: book.title,
+      author: author || "unknown",
+      publishYear: book.first_publish_year || "",
+      // isbn: book.isbn,
+      // lccn: book.lccn,
+      userData: {
+          rating: 0,
+          wantToRead: false,
+          hasRead: false
+      }
+    };
+    formattedSearchResults.push(bookInfo);
+  }
+
+//   function addToList(title) {
+//     let theLi = document.createElement("li");
+//     let theATag = document.createElement("a");
+//     theATag.setAttribute("href", "#");
+
+//     let theText = document.createTextNode(title);
+//     theATag.appendChild(theText);
+//     theLi.appendChild(theATag);
+//     theList.appendChild(theLi);
+//   }
+
+  function addDataToCard(book, list) {
+      console.log('list---', list);
+    //   let idName = 'row'
+    //   if (list === 'has-read') {
+    //       idName = 'has-read'
+    //   }
+    //   if (list === 'want-to-read') {
+    //       idName = 'want-to-read'
+    //   }
+    const cardRow = document.querySelector(`#card-row-${list}`);
+    const hasRead = book.userData.hasRead;
+    let btnClass = 'primary';
+    let hasReadButtonText = 'Mark as Read';
+    if (hasRead) {
+        btnClass = 'secondary'
+        // hasReadButtonText = 'Mark as Unread'
     }
-    formattedSearchResults.push(bookInfo)
-    console.log('search results---',formattedSearchResults);
-    console.log('nook info', bookInfo);
-}
-
-function addToList(title) {
-    let theLi = document.createElement('li');
-    let theATag = document.createElement('a');
-    theATag.setAttribute('href', '#')
-
-    let theText = document.createTextNode(title)
-    theATag.appendChild(theText)
-    theLi.appendChild(theATag)
-    theList.appendChild(theLi)
-}
-
-function addDataToCard(book) {
-    const rows = document.querySelector('#card-row');
-    const cardDiv = document.querySelector('.card-div');
-
-    const bookCard = document.querySelector('.search-card');
-    // const bookCardBody = document.querySelector('.card-body');
-    // const bookCardTitle = document.querySelector('.card-title');
-    // bookCardTitle.textContent = resultsArr.title
 
 
-
-    console.log('bookj title', book.title);
+    console.log("bookj title", book.title);
 
     let cardTemplate = `
-    <div class="card search-card m-3" style="width: 18rem;">
-    <div class="card-body">
+    <div class="card search-card m-3"  style="width: 18rem;">
+    <div class="card-body" id=${book.id}>
       <h5 class="card-title">${book.title}</h5>
       <p class="card-text">Written by ${book.author}. First published in ${book.publishYear}</p>
 
-      <a href="#" class="btn btn-primary" id="mark-as-read">Mark as Read</a>
-      <a href="#" class="btn btn-primary" id="want-to-read">Want to Read</a>
+      <button class="btn btn-${btnClass}" type="submit" id="mark-as-read">${hasReadButtonText}</button>
+      <button class="btn btn-primary" type="submit" id="want-to-read">Want to Read</button>
 
 <form>
 <div class="star-rating">
-<span class="fas fa-star checked" onclick="clicked()"></span>
+<span class="fas fa-star checked"></span>
 <span class="far fa-star" onclick="clicked()"></span>
 <span class="far fa-star" onclick="clicked()"></span>
 </div>
 </form>
 </div>
 
-  </div>`
+  </div>`;
 
-  rows.insertAdjacentHTML("beforeend", cardTemplate);
+  cardRow.insertAdjacentHTML("beforeend", cardTemplate);
+  }
 
+  let cardContainer = document.querySelector('#card-container-search');
+  cardContainer.addEventListener('click', (event) => {
+    console.log('cardContainer clicked');
+    event.preventDefault();
+    console.log('event targer--->', event.target.parentElement);
+    const bookId = event.target.parentElement.getAttribute('id');
+    
+    // getAttribute('id');
+    console.log('bookId--', bookId);
+    const { type, id } = event.target;
+    console.log('type--->', type);
+    console.log('id---->', id);
+  if (id === 'mark-as-read') {
+        setMarkAsRead(bookId, event.target)
+
+    }
+    if (id === 'want-to-read') {
+        setWantToRead(bookId, event.target)
+
+    }
+  
+})
+function setMarkAsRead(bookId, target) {
+    let bookItem = formattedSearchResults.find((i) => i.id == bookId)
+    bookItem.userData.hasRead = true;
+    console.log('bookItem--', bookItem);
+    target.setAttribute('class', 'btn btn-secondary')
+    console.log('target00000-----', target);
+    addDataToHasReadCard(bookItem)
 }
 
-var logID = 'log',
-  log = $('<div id="'+logID+'"></div>');
-$('body').append(log);
+function setWantToRead(bookId, target) {
+    let bookItem = formattedSearchResults.find((i) => i.id == bookId)
+    bookItem.userData.wantToRead = true;
+    target.setAttribute('class', 'btn btn-secondary')
+    addDataToWantToReadCard(bookItem)
+    console.log('bookItem--', bookItem);
+}
+
+function addDataToHasReadCard(book) {
+    const cardRow = document.querySelector('#card-row-has-read');
+    const hasRead = book.userData.hasRead;
+    let btnClass = 'primary';
+    let hasReadButtonText = 'Mark as Read';
+    if (hasRead) {
+        btnClass = 'secondary'
+        // hasReadButtonText = 'Mark as Unread'
+    }
+
+
+    console.log("bookj title", book.title);
+
+    let cardTemplate = `
+    <div class="card search-card m-3"  style="width: 18rem;">
+    <div class="card-body" id=${book.id}>
+      <h5 class="card-title">${book.title}</h5>
+      <p class="card-text">Written by ${book.author}. First published in ${book.publishYear}</p>
+
+<form>
+<div class="star-rating">
+<span class="fas fa-star checked"></span>
+<span class="far fa-star" onclick="clicked()"></span>
+<span class="far fa-star" onclick="clicked()"></span>
+</div>
+</form>
+</div>
+
+  </div>`;
+
+  cardRow.insertAdjacentHTML("beforeend", cardTemplate);
+  
+}
+
+
+
+function addDataToWantToReadCard(book) {
+    const cardRow = document.querySelector('#card-row-want-to-read');
+    const hasRead = book.userData.hasRead;
+    let btnClass = 'primary';
+    let hasReadButtonText = 'Mark as Read';
+    if (hasRead) {
+        btnClass = 'secondary'
+        // hasReadButtonText = 'Mark as Unread'
+    }
+
+
+    console.log("bookj title", book.title);
+
+    let cardTemplate = `
+    <div class="card search-card m-3"  style="width: 18rem;">
+    <div class="card-body" id=${book.id}>
+      <h5 class="card-title">${book.title}</h5>
+      <p class="card-text">Written by ${book.author}. First published in ${book.publishYear}</p>
+      <button class="btn btn-${btnClass}" type="submit" id="mark-as-read">${hasReadButtonText}</button>
+<form>
+<div class="star-rating">
+<span class="fas fa-star checked"></span>
+<span class="far fa-star" onclick="clicked()"></span>
+<span class="far fa-star" onclick="clicked()"></span>
+</div>
+</form>
+</div>
+
+  </div>`;
+
+  cardRow.insertAdjacentHTML("beforeend", cardTemplate);
+  
+}
+
+  var logID = "log",
+    log = $('<div id="' + logID + '"></div>');
+  $("body").append(log);
   $('[type*="radio"]').change(function () {
     var me = $(this);
-    log.html(me.attr('value'));
+    log.html(me.attr("value"));
   });
 
   const btn = document.querySelector(".submit-rating");
-const thanksmsg = document.querySelector(".thanks-msg");
-const starRating = document.querySelector(".star-input");
-// Success msg show/hide
-btn.onclick = () => {
+  const thanksmsg = document.querySelector(".thanks-msg");
+  const starRating = document.querySelector(".star-input");
+  // Success msg show/hide
+  btn.onclick = () => {
     starRating.style.display = "none";
     thanksmsg.style.display = "table";
     return false;
-};
+  };
 
-function clicked() {
-    console.log('clicked');
-}
-
-})(window)
+  function clicked() {
+    console.log("clicked");
+  }
+})(window);
